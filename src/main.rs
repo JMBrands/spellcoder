e4eb095aa8c881d1847a36b4c01d6015448d875f
JMBrands <jonar@ultramagic.net>
2024-10-27 18:13:50 +0100

    added Player class

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,80 @@
-use std::{f32::consts::PI};
+use std::f32::consts::PI;
 
 use raylib::prelude::*;
 
-const SPEED: f32 = 1.0;
+const SPEED: f32 = 32.0;
+
+struct Player {
+    position: Vector3,
+    camera: Camera3D,
+    size: Vector3,
+}
+impl Player {
+    fn new(position: Vector3) -> Self {
+        Player {
+            position,
+            camera: Camera3D::perspective(
+                Vector3 { x: position.x + 8.0, y: position.y + 24.0, z: position.z + 8.0 },
+                Vector3 { x: position.x + 9.0, y: position.y + 24.0, z: position.z + 8.0 },
+                Vector3::up(),
+                70.0
+            ),
+            size: Vector3 {
+                x: 16.0,
+                y: 32.0,
+                z: 16.0,
+            },
+        }
+    }
+    // get normalized vector of the direction of the camera
+    fn get_look_direction(&self) -> Vector3 {
+        (self.camera.target - self.camera.position).normalized()
+    }
+    
+    // set direction of the camera
+    fn set_look_direction(&mut self, dir: Vector3) {
+        self.camera.target = self.camera.position + dir;
+    }
+    
+    // get direction of the camera as yaw & pitch
+    fn get_look_direction_vec2(&self) -> Vector2 {
+        let vec3 = self.get_look_direction();
+        let h_vec2 = Vector2 {
+            x: vec3.x,
+            y: vec3.z,
+        }
+        .normalized();
+        Vector2 {
+            x: h_vec2.y.acos() * h_vec2.x.signum(),
+            y: vec3.y.asin(),
+        }
+    }
+    
+    // set direction of the camera as yaw & pitch
+    fn set_look_direction_vec2(&mut self, dir: Vector2) {
+        let h_vec2 = Vector2 {
+            x: dir.x.sin(),
+            y: dir.x.cos(),
+        }
+        .normalized()
+            * dir.y.cos().abs();
+        self.set_look_direction(
+            Vector3 {
+                x: h_vec2.x,
+                y: dir.y.sin(),
+                z: h_vec2.y,
+            }
+            .normalized(),
+        );
+    }
+    
+    // move camera without changing yaw & pitch
+    fn move_camera(&mut self, delta: Vector3) {
+        self.position += delta;
+        self.camera.position += delta;
+        self.camera.target += delta;
+    }
+}
 
 struct Voxel {
     x: i64,
@@ -31,90 +103,15 @@ impl VoxelDraw for RaylibMode3D<'_, RaylibDrawHandle<'_>> {
     }
 }
 
-// more functionaloty for the camera
-trait SpellCoderCamera {
-    fn get_look_direction(&self) -> Vector3;
-    fn get_look_direction_vec2(&self) -> Vector2;
-    fn set_look_direction(&mut self, dir: Vector3);
-    fn set_look_direction_vec2(&mut self, dir: Vector2);
-    fn move_camera(&mut self, delta: Vector3);
-}
-impl SpellCoderCamera for Camera3D {
-    // get normalized vector of the direction of the camera
-    fn get_look_direction(&self) -> Vector3 {
-        (self.target - self.position).normalized()
-    }
-
-    // set direction of the camera
-    fn set_look_direction(&mut self, dir: Vector3) {
-        self.target = self.position + dir;
-    }
-
-    // get direction of the camera as yaw & pitch
-    fn get_look_direction_vec2(&self) -> Vector2 {
-        let vec3 = self.get_look_direction();
-        let h_vec2 = Vector2 {
-            x: vec3.x,
-            y: vec3.z,
-        }
-        .normalized();
-        Vector2 {
-            x: h_vec2.y.acos() * h_vec2.x.signum(),
-            y: vec3.y.asin(),
-        }
-    }
-
-    // set direction of the camera as yaw & pitch
-    fn set_look_direction_vec2(&mut self, dir: Vector2) {
-        let h_vec2 = Vector2 {
-            x: dir.x.sin(),
-            y: dir.x.cos(),
-        }
-        .normalized()
-            * dir.y.cos().abs();
-        self.set_look_direction(
-            Vector3 {
-                x: h_vec2.x,
-                y: dir.y.sin(),
-                z: h_vec2.y,
-            }
-            .normalized(),
-        );
-    }
-
-    // move camera without changing yaw & pitch
-    fn move_camera(&mut self, delta: Vector3) {
-        self.position += delta;
-        self.target += delta;
-    }
-}
 
 fn main() {
     // set up window
     let (mut rl, thread) = raylib::init().size(640, 480).title("Spellcoder").build();
     rl.set_target_fps(60);
     rl.disable_cursor();
-
-    // set up camera
-    let mut camera = Camera3D::perspective(
-        Vector3 {
-            x: 3.0,
-            y: 2.0,
-            z: 3.0,
-        },
-        Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        },
-        Vector3 {
-            x: 0.0,
-            y: 1.0,
-            z: 0.0,
-        },
-        70.0,
-    );
-    camera.set_look_direction_vec2(Vector2 {
+    // set up player
+    let mut player = Player::new(Vector3::zero());
+    player.set_look_direction_vec2(Vector2 {
         x: -3.0 * PI / 4.0,
         y: -0.5,
     });
@@ -127,14 +124,14 @@ fn main() {
 
         // process input
         let mdelta = rl.get_mouse_delta(); // get mouse input
-        let mut cam_angle = camera.get_look_direction_vec2(); // get current yaw & pitch
+        let cam_angle = player.get_look_direction_vec2(); // get current yaw & pitch
 
         if (mdelta != Vector2 { x: 0.0, y: 0.0 }) {
             let new_cam_angle = Vector2 {
                 x: cam_angle.x - mdelta.x / 180.0 * PI,
                 y: cam_angle.y - (mdelta.y / 180.0 * PI),
             }; // construct new yaw & pitch vector
-            camera.set_look_direction_vec2(new_cam_angle);
+            player.set_look_direction_vec2(new_cam_angle);
         }
 
         let mut movement = Vector2::zero();
@@ -153,15 +150,23 @@ fn main() {
 
         if movement != Vector2::zero() {
             let cam_vec2 = Vector2 {
-                x: camera.get_look_direction().x,
-                y: camera.get_look_direction().z,
-            }.normalized();
+                x: player.get_look_direction().x,
+                y: player.get_look_direction().z,
+            }
+            .normalized();
             movement.normalize();
             movement = Vector2::new(
                 movement.x * cam_vec2.x - movement.y * cam_vec2.y,
-                movement.y * cam_vec2.x + movement.x * cam_vec2.y
-            ).normalized() * SPEED * delta;
-            camera.move_camera(Vector3 { x: movement.x, y: 0.0, z: movement.y });
+                movement.y * cam_vec2.x + movement.x * cam_vec2.y,
+            )
+            .normalized()
+                * SPEED
+                * delta;
+            player.move_camera(Vector3 {
+                x: movement.x,
+                y: 0.0,
+                z: movement.y,
+            });
         }
 
         // set up drawing
@@ -170,10 +175,10 @@ fn main() {
         d.clear_background(Color::BLACK);
         // use d for 2d drawing here (background)
 
-        let mut d3d = d.begin_mode3D(camera);
+        let mut d3d = d.begin_mode3D(player.camera);
 
         // use d3d for 3d drawing here
-        d3d.draw_grid(10, 1.0);
+        d3d.draw_grid(32, 1.0);
         d3d.draw_voxel(Voxel {
             x: 0,
             y: 0,
@@ -186,6 +191,8 @@ fn main() {
             z: 0,
             color: Color::from_hex("FF8000").unwrap().into(),
         });
+
+        d3d.draw_bounding_box(BoundingBox {min: player.position, max: player.position + player.size}, Color::WHITE);
         drop(d3d);
         // use d for 2d drawing here (overlay)
 
